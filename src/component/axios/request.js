6af62767017ccc3734df0969c0606637a1a0d808
Peterliang233 <ncuyanping666@126.com>
2022-08-27 16:45:42 +0800

    :feat: 新增路由导航

---
+++ src/component/axios/request.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+
+const BASE_URL = "http://localhost:8080"
+
+const instance = axios.create({
+    timeout: 1000*20,
+    baseURL: BASE_URL,
+})
+
+instance.defaults.headers.post['Content-Type'] = 'application/json';
+
+
+// 添加请求拦截器
+instance.interceptors.request.use(config=>{
+    const token=localStorage.getItem('token') // 获取token
+    if (token) {
+        // 如果有token，将token放入到请求头里面
+        config.headers.Authorization = `Bearer ` + token;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error)
+})
+
+
+// 添加响应拦截器
+instance.interceptors.response.use(response=>{
+    if (response.statusText === "ok") {
+        return Promise.resolve(response.data);
+    }else{
+        return Promise.reject(response.data.msg);
+    }
+}, error => {
+    if (error.response) {
+        if (error.response.status === 401) {
+            this.props.history.push('/login');
+        }
+        return Promise.reject(error);
+    }else{
+        return Promise.reject('请求超时，请重试');
+    }
+})
+
+
+export default instance;
+
